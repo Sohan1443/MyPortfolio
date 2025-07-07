924ed677a7fd0ba981d3a4a65d86d7c140cb8c8f
Sohan1443 <sohan63072@gmail.com>
2025-07-07 13:45:20 +0530

    frist Commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+
+import './App.css'
+
+
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Project from './components/Project'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+
+import Skills from './components/Skills'
+
+
+
+const Router = createBrowserRouter([
+
+  {
+    path: '/',
+    element: <div>
+      <Header />
+      <Home />
+      <Footer />
+
+    </div>,
+  },
+ 
+  {
+    path: '/about',
+    element: <div>
+      <Header />
+      <About />
+      <Footer />
+
+    </div>,
+  },
+  {
+    path: '/skills',
+    element: <div>
+      <Header />
+      <Skills />
+      <Footer />
+
+    </div>,
+  },
+  {
+    path: '/projects',
+    element: <div>
+      <Header />
+      <Project />
+      <Footer />
+
+    </div>,
+  },
+  {
+    path: '/contact',
+    element: <div>
+      <Header />
+      <Contact />
+      <Footer />
+
+    </div>,
+  }
+])
+
+function App() {
+
+  return (
+    <>
+      <RouterProvider router={Router} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+
+import '../styles/Home.css'
+import { FaHandSparkles } from "react-icons/fa";
+import homeImage from '../assets/Sohan.png'; // Adjust the path as necessary
+
+export default function Home() {
+  const words = [
+    "Web developer ",
+    "Front End developer ",
+    "Full Stack Developer ",
+    "Backend Developer "
+  ];
+  const [displayedText, setDisplayedText] = useState("");
+  const [wordIndex, setWordIndex] = useState(0);
+
+  useEffect(() => {
+    setDisplayedText("");
+    let charIndex = 0;
+    const word = words[wordIndex];
+    const interval = setInterval(() => {
+      setDisplayedText(word.slice(0, charIndex + 1));
+      charIndex++;
+      if (charIndex > word.length - 1) {
+        clearInterval(interval);
+        setTimeout(() => {
+          setWordIndex((prev) => (prev + 1) % words.length);
+        }, 700);
+      }
+    }, 150);
+    return () => clearInterval(interval);
+  }, [wordIndex]);
+
+  return (
+    <>
+      <div className="home-container">
+        <div className="home-content">
+          <h1>Welcome to My Portfolio</h1>
+          <p>Hi <FaHandSparkles className='hiIcon' />, I'm Sohan,<br/>A passionate
+            <span> {displayedText}</span>
+          </p>
+          <p>Explore my projects and skills below.</p>
+          <button className="btn">Get Started</button>
+        </div>
+        <div className="home-image">
+          <img src={homeImage} alt="Home" />
+        </div>
+      </div>
+    </>
+  )
+}
